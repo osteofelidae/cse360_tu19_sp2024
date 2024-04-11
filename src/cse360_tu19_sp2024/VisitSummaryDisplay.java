2d75b3843009ddaca89cab37d665cc86b023a2ba
D. Hamada <118421156+diyahamada@users.noreply.github.com>
2024-04-10 21:36:07 -0700

    ACTUALLY THIS TIME
    - nurse home done
    - visit summaries done
    - under 12 rule implemented

--- src/cse360_tu19_sp2024/VisitSummaryDisplay.java
+++ src/cse360_tu19_sp2024/VisitSummaryDisplay.java
@@ -7,11 +7,13 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
@@ -71,8 +73,8 @@ public class VisitSummaryDisplay extends Application {
         Font labelFont = Font.font("Times New Roman");
 
         FileHandler fh = new FileHandler();
-        String fname = fh.getAttr(username, "First name");
-        String lname = fh.getAttr(username, "Last name");
+        String fname = fh.getAttr("files/users/"+username+".txt", "First name");
+        String lname = fh.getAttr("files/users/"+username+".txt", "Last name");
         Label title = new Label("Visit Summaries - " + fname + " " + lname);
         title.setFont(Font.font("Arial", 24));
         
@@ -294,7 +296,7 @@ public class VisitSummaryDisplay extends Application {
             healField.setText("No previous health risks.");
             
             HashMap<String, String> data = fh.parse(username);
-            if(fh.getAttr(username, "Weight") != null) {
+            if(fh.getAttr("files/users/"+username+".txt", "Weight") != null) {
             	weightField.setText(data.get("Weight"));
                 ftField.setText(data.get("Feet"));
                 inField.setText(data.get("Inches"));
@@ -375,6 +377,14 @@ public class VisitSummaryDisplay extends Application {
             		);
         }else { //nurse
         	setEditable(true);
+        	if(Integer.valueOf(fh.getAttr("files/users/"+username+".txt", "DOB Year")) >= 2012) {
+        		setEditable(false);
+        		System.out.println("Patient too young!");
+                Alert alert = new Alert(AlertType.ERROR);
+            	alert.setHeaderText("Patient is under the age of 12.");
+            	alert.setContentText("No vitals entries may be entered.");
+              	alert.showAndWait();
+        	}
         	buttons.getChildren().addAll(
             		home,
             		save
