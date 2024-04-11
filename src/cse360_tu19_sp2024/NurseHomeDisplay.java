c3e333beca4aaea95b19915d486f88f20bfba21a
D. Hamada <118421156+diyahamada@users.noreply.github.com>
2024-04-10 19:03:57 -0700

    nursehome retrievepatient
    can now retreive a patient!

--- src/cse360_tu19_sp2024/NurseHomeDisplay.java
+++ src/cse360_tu19_sp2024/NurseHomeDisplay.java
@@ -1,24 +1,28 @@
 package cse360_tu19_sp2024;
 
+import java.util.HashMap;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import java.io.File;
 
 
 public class NurseHomeDisplay extends Application {
-
-    TextField emailField, passField, repPassField, firstNameField, lastNameField, middleNameField;
+	TextField firstNameField, lastNameField;
 
     @Override
     public void start(Stage primaryStage) {
@@ -35,10 +39,6 @@ public class NurseHomeDisplay extends Application {
         firstBox.setStyle("-fx-background-color: #f0f0f0; -fx-border-color: black; -fx-border-width: 1px;");
         firstBox.setMinSize(100, 100);
         firstBox.setPadding(new Insets(20));
-
-        emailField = new TextField();
-        passField = new TextField();
-        repPassField = new TextField();
         firstNameField = new TextField();
         lastNameField = new TextField();
 
@@ -118,12 +118,24 @@ public class NurseHomeDisplay extends Application {
         spacer.setMinSize(5, 5);
         spacer.setPadding(new Insets(20));
         
+        Button logoutButton = new Button();
+        logoutButton.setStyle("-fx-background-color: white; -fx-border-color: black; -fx-border-width: 2px;");
+        logoutButton.setPrefWidth(100);
+        logoutButton.setPrefHeight(20);
+        logoutButton.setText("Log Out");
+        logoutButton.setOnAction(new EventHandler<>() {
+            public void handle(ActionEvent event) {
+                System.out.println("Logging out!");
+                LoginDisplay back = new LoginDisplay();
+                back.start(primaryStage);
+            }
+        });
         // Add the gray boxes to the white background VBox
-        whiteBackground.getChildren().addAll(firstBox, spacer, secondBox);
+        whiteBackground.getChildren().addAll(firstBox, spacer, secondBox, logoutButton);
 
         whiteBackground.setPadding(new Insets(20, 20, 50, 20));
         
-        Scene scene = new Scene(whiteBackground, 800, 600);
+        Scene scene = new Scene(whiteBackground, 600, 600);
         primaryStage.setScene(scene);
         primaryStage.show();
         
@@ -137,13 +149,60 @@ public class NurseHomeDisplay extends Application {
         
         enterVisitSummaryLink.setOnAction(new EventHandler<>() {
             public void handle(ActionEvent event) {
-                // TBD
+            	VisitSummaryDisplay visit = new VisitSummaryDisplay('n');
+                System.out.println("Opening Visit Summaries!");
+                visit.setUsername("johndoe");  // TODO temp for testing
+                visit.start(primaryStage);
             }
         });
         
         retrieve.setOnAction(new EventHandler<>() {
             public void handle(ActionEvent event) {
-                // TBD
+            	
+            	String fnameInput = firstNameField.getText();
+            	String lnameInput = lastNameField.getText();
+            	
+            	String monthInput = monthComboBox.getValue().toString();
+            	String dayInput = dayComboBox.getValue().toString();
+            	String yearInput = yearComboBox.getValue().toString();
+            	
+            	
+            	FileHandler fh = new FileHandler();
+                
+                File dir = new File("files/users/");
+                File[] directoryListing = dir.listFiles();
+                System.out.println("check1");
+                boolean found = false;
+                if (directoryListing != null) {
+                	System.out.println("check2");
+                  for (File child : directoryListing) {
+                	  System.out.println("check3" + child.getName());
+                	  HashMap<String, String> data = fh.parse("files/users/"+child.getName());
+                      String fname = data.get("First name");
+                      String lname = data.get("Last name");
+                      String dobMonth = data.get("DOB Month");
+                      String dobDay = data.get("DOB Day");
+                      String dobYear = data.get("DOB Year");
+                      
+                      System.out.println(fname + lname +dobMonth +dobDay + dobYear);
+                      
+                      if(fnameInput.equals(fname) && lnameInput.equals(lname) && monthInput.equals(dobMonth) && dayInput.equals(dobDay) && yearInput.equals(dobYear)){
+                    	  System.out.println("Found!");
+                    	  Alert alert = new Alert(AlertType.CONFIRMATION);
+                    	  alert.setHeaderText("Patient retrieved.");
+                    	  alert.setContentText("You can now enter their visit summary.");
+                    	  alert.showAndWait();
+                    	  found = true;
+        				}
+                   }
+                  if(!found) {
+	                  System.out.println("Not found.");
+	                  Alert alert = new Alert(AlertType.ERROR);
+	            	  alert.setHeaderText("Patient not found.");
+	            	  alert.setContentText("Try again.");
+	            	  alert.showAndWait();
+                  }
+                }
             }
         });
         
